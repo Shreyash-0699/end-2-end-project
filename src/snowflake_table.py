ba67e0ff034e45303e768fe860399fc90266ce7f
Shreyashkar Lal Sahu <109665212+Shreyash-0699@users.noreply.github.com>
2023-05-02 01:54:17 +0530

    Create snowflake_table.py

---
+++ src/snowflake_table.py
@@ -0,0 +1,46 @@
+from utils import (create_connection, get_schema, get_df) 
+import pandas
+import numpy
+import matplotlib
+import boto3
+
+account = os.environ.get('account_name')
+user_key = os.environ.get('user_name')
+pass_key = os.environ.get('pass_key')
+warehouse = os.environ.get('warehouse')
+
+access_key = os.environ.get('access')
+secret_key = os.environ.get('secret')
+bucket = os.environ.get('bucket')
+
+s3 = boto3.client('s3', aws_access_key_id = access_key, aws_secret_access_key = secret_key)
+
+df_original = get_df(bucket, 'original_clean.csv', s3)
+df_customer = get_df(bucket, 'customer_clean.csv', s3)
+df_transaction = get_df(bucket, 'transaction_clean.csv', s3)
+df_product_details = get_df(bucket, 'product_detail.csv', s3)
+df_customer_details = get_df(bucket, 'customer_detail.csv', s3)
+
+def create_s3_snowflake_table(account_name, user_nam, password, warehouse_name, df, table_name):
+  conn, cursor = create_connection(account, user_key, pass_key, warehouse)
+  
+  col_type, values = get_schema(df)
+  
+  cursor.execute("USE clv")
+  cursor.execute(f"DROP TABLE IF EXISTS {table_name}")
+  cursor.execute(f"CREATE_TABLE {table_name} ({col_type})")
+  
+  for index, row in df.iterrows():
+    sql = f"INSERT INTO {table_name} VALUES ({values})"
+    cursor.execute(sql, tuple(row))
+    conn.commit()
+    
+  cursor.close()
+  conn.close()
+  
+create_s3_snowflake_table(account, user_key, pass_key, warehouse, df_original, ORIGINAL)
+create_s3_snowflake_table(account, user_key, pass_key, warehouse, df_customer, CUSTOMER)
+create_s3_snowflake_table(account, user_key, pass_key, warehouse, df_transaction, TRANSACTION)
+create_s3_snowflake_table(account, user_key, pass_key, warehouse, df_product_details, PRODUCT_DETAILS)
+create_s3_snowflake_table(account, user_key, pass_key, warehouse, df_customer_details, CUSTOMER_DETAILS)
+  
