6191f0c8821426aba1da38f67b6d00b801022ecf
Shreyashkar Lal Sahu <109665212+Shreyash-0699@users.noreply.github.com>
2023-05-09 13:26:49 +0530

    Update snowflake_table.py

--- src/snowflake_table.py
+++ src/snowflake_table.py
@@ -46,9 +46,9 @@ def create_s3_snowflake_table(account_name, user_name, password, warehouse_name,
   
 # create_s3_snowflake_table(account, user_key, pass_key, warehouse, df_original, "ORIGINAL")
 
-create_s3_snowflake_table(account, user_key, pass_key, warehouse, df_customer, "CUSTOMER")
+# create_s3_snowflake_table(account, user_key, pass_key, warehouse, df_customer, "CUSTOMER")
 
-# create_s3_snowflake_table(account, user_key, pass_key, warehouse, df_transaction, "TRANSACTION")
+create_s3_snowflake_table(account, user_key, pass_key, warehouse, df_transaction, "TRANSACTION")
 
 # create_s3_snowflake_table(account, user_key, pass_key, warehouse, df_product_details, "PRODUCT_DETAILS")
 
